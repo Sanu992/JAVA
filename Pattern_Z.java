18e160f7fdebe0132558e73db0940951c0692c1e
Saavan Shetty S <babishetty899@gmail.com>
2026-07-13 14:54:27 +0530

    Add files via upload

---
+++ Pattern_Z.java
@@ -0,0 +1,29 @@
+package pattern;
+
+import java.util.Scanner;
+
+public class Pattern_Z {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter number of rows");
+		int rows = sc.nextInt(); 
+		System.out.println("Enter number of columns");							// Must be an odd number for a symmetrical middle line
+        int cols = sc.nextInt();
+
+        for (int i = 1; i <=rows; i++) {
+            for (int j = 1; j <=cols; j++) {
+                // Condition to print stars for the letter D
+                if (i==1 || i==rows ||i+j==rows+1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            // Move to the next line after completing each row
+            System.out.println();
+	}
+   }
+
+}
